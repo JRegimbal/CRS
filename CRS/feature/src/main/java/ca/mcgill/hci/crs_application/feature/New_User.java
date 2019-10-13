5730d216b618cafe82fceb9b85fc80198d5cebe5
Juliette Regimbal <juliette.regimbal@gmail.com>
2019-10-13 14:47:14 -0400

    Add button to link to user guide

--- CRS/feature/src/main/java/ca/mcgill/hci/crs_application/feature/New_User.java
+++ CRS/feature/src/main/java/ca/mcgill/hci/crs_application/feature/New_User.java
@@ -1,11 +1,15 @@
 package ca.mcgill.hci.crs_application.feature;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.net.URI;
+import java.net.URL;
 
 public class New_User extends AppCompatActivity {
 
@@ -23,6 +27,15 @@ public class New_User extends AppCompatActivity {
             }
         });
 
+        Button guideButton = findViewById(R.id.userGuideButton);
+        guideButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(getString(R.string.user_guide_url)));
+                startActivity(intent);
+            }
+        });
 
     }
 }
